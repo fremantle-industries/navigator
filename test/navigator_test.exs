b9880766fa0979c2b94f0436c0ba0e444d09ae5d
Alex <alex+git@fremantle.io>
2021-05-14 16:04:54 -0700

    WIP: Improve testing

--- test/navigator_test.exs
+++ test/navigator_test.exs
@@ -1,4 +1,24 @@
 defmodule NavigatorTest do
-  use ExUnit.Case
+  use Navigator.ViewCase
   doctest Navigator
+
+  defmodule StorefrontWeb.Endpoint do
+    def config(:otp_app), do: :storefront
+  end
+
+  # defmodule AdminWebEndpoint do
+  #   def config(:otp_app), do: :admin
+  # end
+
+  describe ".render/2 horizontal.html" do
+    test "displays links configured for the application endpoint" do
+      conn = %Plug.Conn{private: %{phoenix_endpoint: StorefrontWeb.Endpoint}}
+      html = render(Navigator, "horizontal.html", conn: conn)
+
+      assert assert_html(html, "a", max: 5)
+    end
+  end
+
+  # describe ".render/2 vertical.html" do
+  # end
 end
---
+++ test/support/view_case.ex
@@ -0,0 +1,18 @@
+defmodule Navigator.ViewCase do
+  use ExUnit.CaseTemplate
+  use Phoenix.HTML
+
+  def render(module, template, assigns) do
+    module
+    |> apply(:render, [template, assigns])
+    |> safe_to_string()
+  end
+
+  using do
+    quote do
+      use AssertHTML
+
+      import Navigator.ViewCase
+    end
+  end
+end
