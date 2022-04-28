f3b6a550559a8da63f433a421bdc0660fdee8589
Alex Kwiatkowski <alex+git@fremantle.io>
2022-04-28 01:00:39 -0700

    render horizontal active class for active nodes

--- test/navigator/layouts/horizontal_test.exs
+++ test/navigator/layouts/horizontal_test.exs
@@ -9,23 +9,36 @@ defmodule Navigator.Layouts.HorizontalTest do
   end
 
   test "renders links for the endpoint otp app of the connection" do
-    conn = struct(Plug.Conn, request_path: "/", private: %{phoenix_endpoint: Support.Endpoint})
+    conn = struct(Plug.Conn, request_path: "/orders", private: %{phoenix_endpoint: Support.Endpoint})
 
     rendered = render_to_string(Navigator, "horizontal.html", conn: conn)
     assert_html rendered, ~r{Orders}
     assert_html rendered, ~r{Admin}
+    assert_html rendered, "a.active", count: 1
     refute_html rendered, ~r{Users}
     refute_html rendered, ~r{Settings}
   end
 
-  test "renders child links when the request_path matches the to path" do
+  test "renders empty when there are no matches" do
+    conn = struct(Plug.Conn, request_path: "/", private: %{phoenix_endpoint: Support.Endpoint})
+
+    rendered = render_to_string(Navigator, "horizontal.html", conn: conn)
+    refute_html rendered, ~r{Orders}
+    refute_html rendered, ~r{Admin}
+    refute_html rendered, ~r{Users}
+    refute_html rendered, ~r{Settings}
+    refute_html rendered, "a.active"
+  end
+
+  test "renders links without children when the request_path matches the parent to path" do
     conn = struct(Plug.Conn, request_path: "/admin", private: %{phoenix_endpoint: Support.Endpoint})
 
     rendered = render_to_string(Navigator, "horizontal.html", conn: conn)
     assert_html rendered, ~r{Orders}
     assert_html rendered, ~r{Admin}
-    assert_html rendered, ~r{Users}
-    assert_html rendered, ~r{Settings}
+    assert_html rendered, "a.active", count: 1
+    refute_html rendered, ~r{Users}
+    refute_html rendered, ~r{Settings}
   end
 
   test "renders parent & sibling links when the request_path equals the to path" do
@@ -36,6 +49,7 @@ defmodule Navigator.Layouts.HorizontalTest do
     assert_html rendered, ~r{Admin}
     assert_html rendered, ~r{Users}
     assert_html rendered, ~r{Settings}
+    assert_html rendered, "a.active", count: 2
   end
 
   test "renders parent & sibling links when request_path starts with the to path" do
@@ -46,5 +60,6 @@ defmodule Navigator.Layouts.HorizontalTest do
     assert_html rendered, ~r{Admin}
     assert_html rendered, ~r{Users}
     assert_html rendered, ~r{Settings}
+    assert_html rendered, "a.active", count: 2
   end
 end
